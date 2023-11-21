d7c757d8e49da41a4db0eae77d1ac6f0d1ab387a
James Natulan <jamesnatulan@gmail.com>
2023-11-22 05:25:42 +0800

    Fixed setup issues

--- setup.py
+++ setup.py
@@ -1,2 +1,24 @@
-import setuptools
-setuptools.setup()
+from setuptools import setup, find_packages
+
+REQ_LOCATION = "requirements.txt"
+
+def get_requirements():
+    with open(REQ_LOCATION, encoding="utf-8") as f:
+        requirements = f.read().splitlines()
+        return [r for r in requirements if not r.startswith("--") and not r.startswith("#")]
+
+
+setup(
+    name="ram",
+    version="0.0.1",
+    description="Recognize Anything Plus Model, Recognize Anything Model and Tag2Text Model",
+    install_requires=get_requirements(),
+    include_package_data=True,
+    packages=["data", "models", "utils"],
+    package_dir={
+        "": ".",
+        "data": "ram/data",
+        "models": "ram/models",
+        "utils": "ram/utils",
+    },
+)
